7177564887679de82864479ba1cf68d5fec4bda8
Amin Babar <amin.babar1@gmail.com>
2021-08-24 11:40:19 -0400

    worked on actions

---
+++ todos/frontend/todo_redux.jsx
@@ -0,0 +1,9 @@
+
+import ReactDOM from "react-dom";
+import React from "react";
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    let content = document.getElementById("content");
+
+    ReactDOM.render(<h1> Todos App</h1>, content);
+});
