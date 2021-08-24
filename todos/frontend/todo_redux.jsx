e2a4d1946bec95f43bd3a0853ef7fce52582b19e
cindyjiang <cindyj301@gmail.com>
2021-08-24 11:11:33 -0500

    finished checking initial state

--- todos/frontend/todo_redux.jsx
+++ todos/frontend/todo_redux.jsx
@@ -1,9 +1,12 @@
-
 import ReactDOM from "react-dom";
 import React from "react";
+import configureStore from './store/store'
+import { receiveTodo }from './actions/todo_actions'
 
 document.addEventListener("DOMContentLoaded", ()=> {
     let content = document.getElementById("content");
-
     ReactDOM.render(<h1> Todos App</h1>, content);
+
+    window.store = configureStore();
+    window.receiveTodo = receiveTodo;
 });
